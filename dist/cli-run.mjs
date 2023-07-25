925d76b7b63fef71e2e69d98ce69c7e85e8f166c
Angel Hernandez <angel.hernandez.12@live.com>
2023-07-25 16:58:24 -0400

    Desperate attempt to solve this

--- dist/cli-run.mjs
+++ dist/cli-run.mjs
@@ -1,2 +1,6 @@
-process._startTime = Date.now();
-import('./cli.mjs').then((r) => (r.default || r).main());
+import jiti from "file:///C:/Users/AngelHdz/Documents/Programming/TypeScript/projects/nuxi/scaffold/node_modules/.pnpm/jiti@1.19.1/node_modules/jiti/lib/index.js";
+
+/** @type {import("C:/Users/AngelHdz/Documents/Programming/TypeScript/projects/nuxi/scaffold/src/cli-run")} */
+const _module = jiti(null, { interopDefault: true, esmResolve: true })("C:/Users/AngelHdz/Documents/Programming/TypeScript/projects/nuxi/scaffold/src/cli-run.ts");
+
+export default _module;
---
+++ pages/f.vue
@@ -0,0 +1,8 @@
+<script lang="ts" setup>
+</script>
+      
+<template>
+  <main>
+    <span>F Page</span>
+  </main>
+</template>
