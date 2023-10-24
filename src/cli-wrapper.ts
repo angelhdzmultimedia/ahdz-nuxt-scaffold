f303a847bb211a01b8177306b3d16a98a1b4b964
angelhdzmultimedia <angel.hernandez.12@live.com>
2023-10-24 19:30:59 -0400

    Hmmm

--- src/cli-wrapper.ts
+++ src/cli-wrapper.ts
@@ -1,18 +1,16 @@
 import {parseArgs} from 'node:util'
 import { commands } from './commands'
-import consola from 'consola'
 import { runCommand } from 'citty'
 
 async function main() {
   const {positionals: [commandName, ...rest]} = parseArgs({allowPositionals: true})
-  const esm = await commands[commandName as keyof typeof commands]()
 
   if (!(commandName in commands)) {
     throw new Error(`Command "${commandName}" not available.`)
   }
 
   await runCommand(await commands[commandName as keyof typeof commands](), {
-    rawArgs: rest
+    rawArgs: rest,
   })
 }
 
